b058f0cd24f1314d8bea2801c1c5a59fec4e3cfb
Rachel Freeland <rachel.freeland1@gmail.com>
2021-08-18 02:44:08 -0500

    Feature 2 completed and uploaded

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return(
-      <div>
+      <>
         <h1>The Famous Gallery of Horns</h1>
-      </div>
+      </>
     );
   }
 }
