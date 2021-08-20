d11b42b1a1b1d4a83761ad03de4bb8895f58d4f6
Rachel Freeland <rachel.freeland1@gmail.com>
2021-08-20 01:14:11 -0500

    Fixed modal so that it will now display

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import CardColumns from 'react-bootstrap/CardColumns';
 import HornedBeast from './HornedBeast.js';
 
 
 class Main extends React.Component {
   render() {
     return(
-      <>
+      <CardColumns>
         {this.props.beastData.map( (beast, i) => {
           return (<HornedBeast
             key={i}
+            beastkey={i}
             imgUrl={beast.image_url}
             title={beast.title}
             description={beast.description}
             keyword={beast.keyword}
-            horns={beast.horns} />
+            horns={beast.horns}
+            showModal={this.props.showModal} />
           );
         })
         }
-      </>
+      </CardColumns>
     );
   }
 }
